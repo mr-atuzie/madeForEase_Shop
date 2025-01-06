8ad271aa5062c989910cd299625623fd9fa477f3
rex atuzie <rexatuzie@gmail.com>
2025-01-06 04:10:17 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <p className=" text-yellow-500">madeFoEase_shop</p>
+      <small className=" text-blue-500">coming soon</small>
+    </>
+  );
+}
+
+export default App;

ab9e64b13a083f8778e2a629c45451079bd3a72a
rex atuzie <rexatuzie@gmail.com>
2025-01-07 10:52:42 +0100

    ---

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-icons/io", // Explicitly mark react-icons/io as external
-      ],
-    },
-  },
 });
